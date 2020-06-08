0e0ae69441483785d233d1c3f099027a3788ce52
lechercheur <merzouk1985@gmail.com>
2020-06-08 18:05:38 +0200

    add node server with sqlite to log nd register user

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors  = require('cors')
+
+
+const api = require('./api')
+
+const app = express()
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(api);
+
+
+app.listen(3000, () => {
+    console.log(`Server started on port 3000`);
+});
